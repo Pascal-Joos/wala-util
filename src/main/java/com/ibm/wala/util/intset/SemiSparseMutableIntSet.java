bc33d214d8e20c79619e5593ffd2cf91d2a269ab
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 17:55:31 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression densePart is @Nullable' /home/vscode/nullness-benchmarks/wala-util/src/main/java/com/ibm/wala/util/intset/SemiSparseMutableIntSet.java:493     return densePart.getOffset() <= i && densePart.length() > i;

--- src/main/java/com/ibm/wala/util/intset/SemiSparseMutableIntSet.java
+++ src/main/java/com/ibm/wala/util/intset/SemiSparseMutableIntSet.java
@@ -488,7 +488,7 @@ public class SemiSparseMutableIntSet implements MutableIntSet {
   }
 
   private boolean inDenseRange(int i) {
-    return densePart.getOffset() <= i && densePart.length() > i;
+    return densePart != null && densePart.getOffset() <= i && densePart.length() > i;
   }
 
   /**
