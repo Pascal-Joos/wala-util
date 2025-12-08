46f35999895eae97980fd3ef5ed659f623a85225
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 10:20:38 +0000

    Revert "fix: 57 - ASSIGN_FIELD_NULLABLE - elements = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 20d402985ef88a70d9fbf52d0f941eb0a5606af7.

--- src/main/java/com/ibm/wala/util/intset/MutableSparseLongSet.java
+++ src/main/java/com/ibm/wala/util/intset/MutableSparseLongSet.java
@@ -77,7 +77,7 @@ public final class MutableSparseLongSet extends SparseLongSet implements Mutable
       }
       if (elements[remove] == value) {
         if (size == 1) {
-          elements = new long[0];
+          elements = null;
           size = 0;
         } else {
           if (remove < size) {
