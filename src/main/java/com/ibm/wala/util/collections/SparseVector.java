b4f1f07ece08207fb7f7fd5450afed304dc5ef8d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 16:19:55 +0000

    fix: Type='WRONG_OVERRIDE_RETURN', message='method returns @Nullable, but superclass method com.ibm.wala.util.collections.IVector.get(int) returns @NonNull' /home/vscode/nullness-benchmarks/wala-util/src/main/java/com/ibm/wala/util/collections/SparseVector.java:53   @Nullable

--- src/main/java/com/ibm/wala/util/collections/SparseVector.java
+++ src/main/java/com/ibm/wala/util/collections/SparseVector.java
@@ -50,13 +50,12 @@ public class SparseVector<T> implements IVector<T>, Serializable {
   /**
    * @see com.ibm.wala.util.intset.IntVector#get(int)
    */
-  @Nullable
   @Override
   @SuppressWarnings("unchecked")
   public T get(int x) {
     int index = indices.getIndex(x);
     if (index == -1) {
-      return null;
+      throw new NoSuchElementException("No element at index " + x);
     } else {
       return (T) data[index];
     }
