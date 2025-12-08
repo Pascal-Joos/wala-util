ca24163cab731c21219fdc904a277bc296c6986c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 09:57:11 +0000

    fix: 39 - DEREFERENCE_NULLABLE - if (!that.contains(x)) { - dereferenced expression that is @Nullable

--- src/main/java/com/ibm/wala/util/intset/BitVectorIntSet.java
+++ src/main/java/com/ibm/wala/util/intset/BitVectorIntSet.java
@@ -378,7 +378,10 @@ public final class BitVectorIntSet implements MutableIntSet {
    * @see com.ibm.wala.util.intset.IntSet#isSubset(com.ibm.wala.util.intset.IntSet)
    */
   @Override
-  public boolean isSubset(@Nullable IntSet that) {
+  public boolean isSubset(IntSet that) {
+    if (that == null) {
+      return isEmpty();
+    }
     if (that instanceof BitVectorIntSet) {
       return bitVector.isSubset(((BitVectorIntSet) that).bitVector);
     } else if (that instanceof SparseIntSet) {
