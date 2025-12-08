680f726194a086d1b1444448d5f1b173c9208461
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 09:38:01 +0000

    Revert "fix: 25 - DEREFERENCE_NULLABLE - while (roots.hasNext()) { - dereferenced expression roots is @Nullable"
    This reverts commit 499bfc835443737e49bae009db91d2b6127306e0.

--- src/main/java/com/ibm/wala/util/graph/traverse/DFSDiscoverTimeIterator.java
+++ src/main/java/com/ibm/wala/util/graph/traverse/DFSDiscoverTimeIterator.java
@@ -101,9 +101,8 @@ public abstract class DFSDiscoverTimeIterator<T> extends ArrayList<T> implements
     } while (!empty());
 
     // search for the next unvisited root.
-    Iterator<? extends T> nonNullRoots = (roots != null) ? roots : EmptyIterator.instance();
-    while (nonNullRoots.hasNext()) {
-      T nextRoot = nonNullRoots.next();
+    while (roots.hasNext()) {
+      T nextRoot = roots.next();
       if (getPendingChildren(nextRoot) == null) {
         push(nextRoot);
         setPendingChildren(nextRoot, getConnected(nextRoot));
