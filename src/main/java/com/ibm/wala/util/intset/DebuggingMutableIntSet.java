d41733152933947962d66062f280213497a4bf5b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 09:59:03 +0000

    fix: 40 - WRONG_OVERRIDE_RETURN - @Nullable - method returns @Nullable, but superclass method com.ibm.wala.util.intset.IntSet.intersection(com.ibm.wala.util.intset.IntSet) returns @NonNull

--- src/main/java/com/ibm/wala/util/intset/DebuggingMutableIntSet.java
+++ src/main/java/com/ibm/wala/util/intset/DebuggingMutableIntSet.java
@@ -13,7 +13,6 @@ package com.ibm.wala.util.intset;
 import com.ibm.wala.util.collections.HashSetFactory;
 import com.ibm.wala.util.debug.Assertions;
 import java.util.Set;
-import javax.annotation.Nullable;
 
 /**
  * This class wraps two concrete {@link MutableIntSet}s behind the standard interface, carrying out
@@ -158,7 +157,6 @@ class DebuggingMutableIntSet implements MutableIntSet {
    *
    * @return a new IntSet which is the intersection of this and that
    */
-  @Nullable
   @Override
   public IntSet intersection(IntSet that) {
     if (that instanceof DebuggingMutableIntSet) {
@@ -171,7 +169,7 @@ class DebuggingMutableIntSet implements MutableIntSet {
       return ppr;
     } else {
       Assertions.UNREACHABLE();
-      return null;
+      return EmptyIntSet.instance;
     }
   }
 
