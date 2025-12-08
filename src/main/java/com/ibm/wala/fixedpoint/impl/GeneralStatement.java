96bfd802b7beb75145be2611bdcb205700a618f7
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 09:09:18 +0000

    fix: 3 - ASSIGN_FIELD_NULLABLE - this.rhs = null; - assigning @Nullable expression to @NonNull field

--- src/main/java/com/ibm/wala/fixedpoint/impl/GeneralStatement.java
+++ src/main/java/com/ibm/wala/fixedpoint/impl/GeneralStatement.java
@@ -79,7 +79,7 @@ public abstract class GeneralStatement<T extends IVariable<T>>
     }
     this.operator = operator;
     this.lhs = lhs;
-    this.rhs = null;
+    this.rhs = makeRHS(0);
     this.hashCode = makeHashCode();
   }
 
