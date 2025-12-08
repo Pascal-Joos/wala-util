485fee94a26aca8e281b1ffee0c3119418acee63
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 09:31:22 +0000

    Revert "fix: 19 - DEREFERENCE_NULLABLE - inEdges.get(dst).remove(getNumber(src)); - dereferenced expression inEdges.get(dst) is @Nullable"
    This reverts commit 95405c3a7a583dd44e4497e14d0e0925b6d8ed0b.

--- src/main/java/com/ibm/wala/util/graph/impl/ExtensionGraph.java
+++ src/main/java/com/ibm/wala/util/graph/impl/ExtensionGraph.java
@@ -105,9 +105,7 @@ public class ExtensionGraph<T> implements NumberedGraph<T> {
           assert hasEdge(src, dst);
           assert !original.hasEdge(src, dst);
           assert containsNode(src) && containsNode(dst);
-          if (inEdges.containsKey(dst)) {
-            inEdges.get(dst).remove(getNumber(src));
-          }
+          inEdges.get(dst).remove(getNumber(src));
           outEdges.get(src).remove(getNumber(dst));
         }
 
