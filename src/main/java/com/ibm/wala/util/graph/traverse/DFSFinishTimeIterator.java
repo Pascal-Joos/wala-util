e55000b37f95298ed4ab8f38bee44bca42355b1c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 09:39:25 +0000

    Revert "fix: 26 - METHOD_NO_INIT - @Initializer - initializer method does not guarantee @NonNull field theNextElement (line 34) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 82e8badc63e0758af16b755c10172477e32de534.

--- src/main/java/com/ibm/wala/util/graph/traverse/DFSFinishTimeIterator.java
+++ src/main/java/com/ibm/wala/util/graph/traverse/DFSFinishTimeIterator.java
@@ -31,7 +31,7 @@ public abstract class DFSFinishTimeIterator<T> extends ArrayList<T> implements I
   private static final long serialVersionUID = 8440061593631309429L;
 
   /** the current next element in finishing time order */
-  @Nullable private T theNextElement;
+  private T theNextElement;
 
   /** an enumeration of all nodes to search from */
   private Iterator<? extends T> roots;
@@ -44,11 +44,7 @@ public abstract class DFSFinishTimeIterator<T> extends ArrayList<T> implements I
   protected void init(Graph<T> G, Iterator<? extends T> nodes) {
     this.G = G;
     roots = nodes;
-    if (roots.hasNext()) {
-      theNextElement = roots.next();
-    } else {
-      theNextElement = null;
-    }
+    if (roots.hasNext()) theNextElement = roots.next();
   }
 
   private boolean empty() {
@@ -62,17 +58,7 @@ public abstract class DFSFinishTimeIterator<T> extends ArrayList<T> implements I
    */
   @Override
   public boolean hasNext() {
-    if (!empty()) {
-      return true;
-    }
-    if (theNextElement == null) {
-      return false;
-    }
-    Iterator<T> pending = getPendingChildren(theNextElement);
-    if (pending == null) {
-      return true;
-    }
-    return roots.hasNext();
+    return (!empty() || (theNextElement != null && getPendingChildren(theNextElement) == null));
   }
 
   @Nullable
@@ -108,9 +94,6 @@ public abstract class DFSFinishTimeIterator<T> extends ArrayList<T> implements I
     }
     if (empty()) {
       T v = theNextElement;
-      if (v == null) {
-        throw new NoSuchElementException();
-      }
       setPendingChildren(v, getConnected(v));
       push(v);
     }
@@ -132,9 +115,7 @@ public abstract class DFSFinishTimeIterator<T> extends ArrayList<T> implements I
       setPendingChildren(v, (Iterator<T>) EmptyIterator.instance());
 
       // no more children to visit: finished this vertex
-      while (theNextElement != null
-          && getPendingChildren(theNextElement) != null
-          && roots.hasNext()) {
+      while (getPendingChildren(theNextElement) != null && roots.hasNext()) {
         theNextElement = roots.next();
       }
 
