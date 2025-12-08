7f207f0e6e63fde29c677743e2ad1df303ead624
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 09:36:39 +0000

    Revert "fix: 24 - UNBOX_NULLABLE - return (t1 - t2); - unboxing of a @Nullable value - t2"
    This reverts commit 7379654007c0519089ed9aeb6907053840adf618.

--- src/main/java/com/ibm/wala/util/graph/traverse/DFS.java
+++ src/main/java/com/ibm/wala/util/graph/traverse/DFS.java
@@ -131,10 +131,8 @@ public class DFS {
       }
       Integer t1 = order.get(o1);
       Integer t2 = order.get(o2);
-      if (t1 == null || t2 == null) {
-        throw new IllegalStateException("node has not been ordered");
-      }
-      return t1.intValue() - t2.intValue();
+      // throws an exception if either node has not been ordered
+      return (t1 - t2);
     }
   }
 
