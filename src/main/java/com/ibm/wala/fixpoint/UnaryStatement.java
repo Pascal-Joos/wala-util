5b2a793d10a611b3d444d983da0f60f0921f3a5c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 09:10:41 +0000

    Revert "fix: 4 - ASSIGN_FIELD_NULLABLE - this.lhs = lhs; - assigning @Nullable expression to @NonNull field"
    This reverts commit 42cf2135261a5401b18719f00fec215900ce37e8.

--- src/main/java/com/ibm/wala/fixpoint/UnaryStatement.java
+++ src/main/java/com/ibm/wala/fixpoint/UnaryStatement.java
@@ -17,7 +17,7 @@ public abstract class UnaryStatement<T extends IVariable<T>>
     extends AbstractStatement<T, UnaryOperator<T>> {
 
   /** The operands */
-  @Nullable protected final T lhs;
+  protected final T lhs;
 
   protected final T rhs;
 
@@ -38,7 +38,6 @@ public abstract class UnaryStatement<T extends IVariable<T>>
    * @return the lattice cell this equation computes
    */
   @Override
-  @Nullable
   public T getLHS() {
     return lhs;
   }
