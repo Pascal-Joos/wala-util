0a8cfc6881aa5e6d648bde11ccd8a4b9c85aff78
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 17:18:51 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression set is @Nullable' /home/vscode/nullness-benchmarks/wala-util/src/main/java/com/ibm/wala/util/intset/MutableSharedBitVectorIntSet.java:653         if (!set.isEmpty()) {

--- src/main/java/com/ibm/wala/util/intset/MutableSharedBitVectorIntSet.java
+++ src/main/java/com/ibm/wala/util/intset/MutableSharedBitVectorIntSet.java
@@ -648,6 +648,9 @@ public class MutableSharedBitVectorIntSet implements MutableIntSet {
   }
 
   private boolean addAllInternal(@Nullable SparseIntSet set) {
+    if (set == null) {
+      return false;
+    }
     if (privatePart == null) {
       if (sharedPart == null) {
         if (!set.isEmpty()) {
