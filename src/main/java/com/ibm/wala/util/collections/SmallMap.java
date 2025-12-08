b8286f98cb2aa8ab298c2c71c97a123b19626fa5
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 09:17:42 +0000

    fix: 9 - FIELD_NO_INIT - private Object[] keysAndValues; - @NonNull field keysAndValues not initialized

--- src/main/java/com/ibm/wala/util/collections/SmallMap.java
+++ src/main/java/com/ibm/wala/util/collections/SmallMap.java
@@ -34,7 +34,8 @@ public class SmallMap<K, V> implements Map<K, V> {
   // this Map contains keysAndValues.length / 2 entries.
   // in the following array, entries 0 ... keysAndValues.length/2 - 1 are keys.
   // entries keysAndValues.length/2 .. keysAndValues.length are values.
-  private Object[] keysAndValues;
+
+  private Object[] keysAndValues = new Object[0];
 
   /*
    */
@@ -83,7 +84,7 @@ public class SmallMap<K, V> implements Map<K, V> {
 
   @Override
   public boolean isEmpty() {
-    return (keysAndValues == null);
+    return size() == 0;
   }
 
   @Override
@@ -186,7 +187,7 @@ public class SmallMap<K, V> implements Map<K, V> {
 
   @Override
   public void clear() {
-    keysAndValues = null;
+    keysAndValues = new Object[0];
   }
 
   @Override
