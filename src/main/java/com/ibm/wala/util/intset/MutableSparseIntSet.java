a1a1e8bd7f930dd375ca7337f5eacc10c1c4ac77
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 10:13:22 +0000

    fix: 51 - ASSIGN_FIELD_NULLABLE - elements = null; - assigning @Nullable expression to @NonNull field

--- src/main/java/com/ibm/wala/util/intset/MutableSparseIntSet.java
+++ src/main/java/com/ibm/wala/util/intset/MutableSparseIntSet.java
@@ -78,7 +78,6 @@ public class MutableSparseIntSet extends SparseIntSet implements MutableIntSet {
       }
       if (elements[remove] == value) {
         if (size == 1) {
-          elements = null;
           size = 0;
         } else {
           if (remove < size) {
