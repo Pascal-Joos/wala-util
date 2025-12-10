f33974f1a8edce3e4a5b42d3970e64be7a4466e0
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 14:30:32 +0000

    fix: 32 - DEREFERENCE_NULLABLE - if (d.getTotalSize() > 10000) { - dereferenced expression d is @Nullable

--- src/main/java/com/ibm/wala/util/heapTrace/HeapTracer.java
+++ src/main/java/com/ibm/wala/util/heapTrace/HeapTracer.java
@@ -665,9 +665,9 @@ public class HeapTracer {
       for (Field field : sortedDemo) {
         Object root = field;
         Demographics d = roots.get(root);
-        if (d.getTotalSize() > 10000) {
+        if (d != null && d.getTotalSize() > 10000) {
           result.append(" root: ").append(root).append('\n');
-          result.append(d);
+          result.append(d == null ? "null" : d.toString());
         }
       }
 
