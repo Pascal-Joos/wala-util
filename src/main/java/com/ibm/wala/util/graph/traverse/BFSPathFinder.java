20669f4611a7d6da9fe1cebe1ee8b24fd1d8db5a
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 14:25:24 +0000

    Revert "fix: 22 - ASSIGN_FIELD_NULLABLE - private HashMap<Object, T> history = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 70f36a5c771218fdca7453ad58e7ee429f0292ad.

--- src/main/java/com/ibm/wala/util/graph/traverse/BFSPathFinder.java
+++ src/main/java/com/ibm/wala/util/graph/traverse/BFSPathFinder.java
@@ -138,7 +138,7 @@ public class BFSPathFinder<T> {
   }
 
   @Nullable private ArrayDeque<T> Q = null;
-  @Nullable private HashMap<Object, T> history = null;
+  private HashMap<Object, T> history = null;
 
   /**
    * @return a List of nodes that specifies the first path found from a root to a node accepted by
@@ -166,9 +166,6 @@ public class BFSPathFinder<T> {
       Iterator<? extends T> children = getConnected(N);
       while (children.hasNext()) {
         T c = children.next();
-        if (history == null) {
-          return null;
-        }
         if (!history.containsKey(c)) {
           Q.addLast(c);
           history.put(c, N);
@@ -183,7 +180,7 @@ public class BFSPathFinder<T> {
    * @return a List which represents a path in the breadth-first search to Q[i]. Q holds the nodes
    *     visited during the BFS, in order.
    */
-  private List<T> makePath(T node, @Nullable Map<Object, T> history) {
+  private List<T> makePath(T node, Map<Object, T> history) {
     ArrayList<T> result = new ArrayList<>();
     T n = node;
     result.add(n);
