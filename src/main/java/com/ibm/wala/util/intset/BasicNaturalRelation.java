d25c5e60ae7975ebb1e4dd20a2c77fb0a1e593a6
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 14:33:15 +0000

    Revert "fix: 36 - DEREFERENCE_NULLABLE - int y = delegateIterator.next(); - dereferenced expression delegateIterator is @Nullable"
    This reverts commit 9e5fc86a9917ff710d4448a3c2e1e08473a3d67a.

--- src/main/java/com/ibm/wala/util/intset/BasicNaturalRelation.java
+++ src/main/java/com/ibm/wala/util/intset/BasicNaturalRelation.java
@@ -232,7 +232,6 @@ public final class BasicNaturalRelation implements IBinaryNaturalRelation, Seria
     public IntPair next() {
       IntPair result = null;
       if (nextIndex == smallStore.length) {
-        assert delegateIterator != null;
         int y = delegateIterator.next();
         result = new IntPair(nextX, y);
         if (!delegateIterator.hasNext()) {
