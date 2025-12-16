245c82a4c2dae36611d01c7ed1528ac5c2fff03c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:14:33 +0000

    fix: 55 - ASSIGN_FIELD_NULLABLE - elements = null; - assigning @Nullable expression to @NonNull field

--- src/main/java/com/ibm/wala/util/intset/MutableSparseLongSet.java
+++ src/main/java/com/ibm/wala/util/intset/MutableSparseLongSet.java
@@ -182,7 +182,7 @@ public final class MutableSparseLongSet extends SparseLongSet implements Mutable
     if (this.isEmpty()) {
       return;
     } else if (that.isEmpty()) {
-      elements = null;
+      elements = new long[0];
       size = 0;
       return;
     } else if (this.equals(that)) {
@@ -194,7 +194,7 @@ public final class MutableSparseLongSet extends SparseLongSet implements Mutable
       if (that.contains(elements[0])) {
         return;
       } else {
-        elements = null;
+        elements = new long[0];
         size = 0;
         return;
       }
@@ -203,12 +203,14 @@ public final class MutableSparseLongSet extends SparseLongSet implements Mutable
       if (contains(that.elements[0])) {
         if (size > INITIAL_NONEMPTY_SIZE) {
           elements = new long[INITIAL_NONEMPTY_SIZE];
+        } else if (elements == null || elements.length == 0) {
+          elements = new long[1];
         }
         size = 1;
         elements[0] = that.elements[0];
         return;
       } else {
-        elements = null;
+        elements = new long[0];
         size = 0;
         return;
       }
@@ -243,7 +245,7 @@ public final class MutableSparseLongSet extends SparseLongSet implements Mutable
 
     // now compact cr to 'just enough'
     size = ci;
-    elements = cr;
+    elements = (cr == null) ? new long[0] : cr;
     return;
   }
 
