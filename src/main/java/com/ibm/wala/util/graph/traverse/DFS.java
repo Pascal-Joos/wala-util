827bb46e3f4c75f627e87827d9f8aeb305b95a25
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:59:44 +0000

    fix: 24 - UNBOX_NULLABLE - return (t1 - t2); - unboxing of a @Nullable value - t2

--- src/main/java/com/ibm/wala/util/graph/traverse/DFS.java
+++ src/main/java/com/ibm/wala/util/graph/traverse/DFS.java
@@ -132,7 +132,7 @@ public class DFS {
       Integer t1 = order.get(o1);
       Integer t2 = order.get(o2);
       // throws an exception if either node has not been ordered
-      return (t1 - t2);
+      return (t1.intValue() - t2.intValue());
     }
   }
 
