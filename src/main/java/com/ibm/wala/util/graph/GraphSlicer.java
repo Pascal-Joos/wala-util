18d6a6b9327220718988d5499c89f4ea9f3dcf63
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:56:22 +0000

    fix: 16 - DEREFERENCE_NULLABLE - return succs.get(N).size(); - dereferenced expression succs.get(N) is @Nullable

--- src/main/java/com/ibm/wala/util/graph/GraphSlicer.java
+++ src/main/java/com/ibm/wala/util/graph/GraphSlicer.java
@@ -280,6 +280,9 @@ public class GraphSlicer {
             if (!succs.containsKey(N)) {
               setSuccNodes(N);
             }
+            if (succs.get(N) == null) {
+              return 0;
+            }
             return succs.get(N).size();
           }
 
