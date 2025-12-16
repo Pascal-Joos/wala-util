048a4ce7125b78645d27116801aa6128a3369f1b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:06:43 +0000

    Revert "fix: 39 - DEREFERENCE_NULLABLE - if (!that.contains(x)) { - dereferenced expression that is @Nullable"
    This reverts commit 04e6f263ab8d9b3ce31010621155b6c93a6a1b48.

--- src/main/java/com/ibm/wala/util/intset/BitVectorIntSet.java
+++ src/main/java/com/ibm/wala/util/intset/BitVectorIntSet.java
@@ -379,9 +379,6 @@ public final class BitVectorIntSet implements MutableIntSet {
    */
   @Override
   public boolean isSubset(@Nullable IntSet that) {
-    if (that == null) {
-      return false;
-    }
     if (that instanceof BitVectorIntSet) {
       return bitVector.isSubset(((BitVectorIntSet) that).bitVector);
     } else if (that instanceof SparseIntSet) {
