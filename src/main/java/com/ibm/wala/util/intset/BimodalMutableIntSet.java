2a627e1ce7fc133b0b4274cd8050a516da49cb7a
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:05:54 +0000

    Revert "fix: 37 - WRONG_OVERRIDE_RETURN - @Nullable - method returns @Nullable, but superclass method com.ibm.wala.util.intset.IntSet.intersection(com.ibm.wala.util.intset.IntSet) returns @NonNull"
    This reverts commit eb5e5d8a2318955a49a4eb31bdd2b2efcfcdc3d0.

--- src/main/java/com/ibm/wala/util/intset/BimodalMutableIntSet.java
+++ src/main/java/com/ibm/wala/util/intset/BimodalMutableIntSet.java
@@ -154,9 +154,6 @@ public class BimodalMutableIntSet implements MutableIntSet {
   /**
    * @see com.ibm.wala.util.intset.IntSet#intersection(com.ibm.wala.util.intset.IntSet)
    */
-  /**
-   * @see com.ibm.wala.util.intset.IntSet#intersection(com.ibm.wala.util.intset.IntSet)
-   */
   @Nullable
   @Override
   public IntSet intersection(IntSet that) throws UnimplementedError {
@@ -167,7 +164,7 @@ public class BimodalMutableIntSet implements MutableIntSet {
       return impl.intersection(that);
     } else {
       Assertions.UNREACHABLE("Unexpected: " + that);
-      throw new IllegalStateException("Unreachable code executed in intersection");
+      return null;
     }
   }
 
