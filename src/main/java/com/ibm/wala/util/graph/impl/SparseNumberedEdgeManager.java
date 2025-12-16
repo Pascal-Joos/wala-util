60f75e3e6fb1861c469e0f73c17e6b0c77f4f584
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:58:15 +0000

    Revert "fix: 21 - RETURN_NULLABLE - return predecessors.getRelated(nodeManager.getNumber(node)); - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 4b314ecaded3ce6f744987f47eee1e1c0409919e.

--- src/main/java/com/ibm/wala/util/graph/impl/SparseNumberedEdgeManager.java
+++ src/main/java/com/ibm/wala/util/graph/impl/SparseNumberedEdgeManager.java
@@ -145,12 +145,7 @@ public final class SparseNumberedEdgeManager<T> implements NumberedEdgeManager<T
     if (nodeManager.getNumber(node) < 0) {
       throw new IllegalArgumentException("Node not in graph " + node);
     }
-    IntSet x = predecessors.getRelated(nodeManager.getNumber(node));
-    if (x == null) {
-      return EmptyIntSet.instance;
-    } else {
-      return x;
-    }
+    return predecessors.getRelated(nodeManager.getNumber(node));
   }
 
   /**
