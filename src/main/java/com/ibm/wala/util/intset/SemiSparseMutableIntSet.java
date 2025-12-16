3f9c3ba2d3a28011bb5e4217b73e6eefceca9b8b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:16:13 +0000

    Revert "fix: 59 - METHOD_NO_INIT - public SemiSparseMutableIntSet(SemiSparseMutableIntSet set) throws IllegalArgumentException { - initializer method does not guarantee @NonNull field sparsePart (line 26) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit a13e959150066def152f3df3f010547316b4224d.

--- src/main/java/com/ibm/wala/util/intset/SemiSparseMutableIntSet.java
+++ src/main/java/com/ibm/wala/util/intset/SemiSparseMutableIntSet.java
@@ -44,7 +44,6 @@ public class SemiSparseMutableIntSet implements MutableIntSet {
     if (set == null) {
       throw new IllegalArgumentException("set == null");
     }
-    this.sparsePart = MutableSparseIntSet.makeEmpty();
     copySet(set);
   }
 
