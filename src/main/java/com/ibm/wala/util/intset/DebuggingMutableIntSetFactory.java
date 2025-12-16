46967c9490ebd9c29901bb96a8aaf04cfab4d5ab
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:07:41 +0000

    Revert "fix: 41 - WRONG_OVERRIDE_RETURN - @Nullable - method returns @Nullable, but superclass method com.ibm.wala.util.intset.MutableIntSetFactory.makeCopy(com.ibm.wala.util.intset.IntSet) returns @NonNull"
    This reverts commit b54c37a7bb1de8eb1671fcb91b03273f142dd768.

--- src/main/java/com/ibm/wala/util/intset/DebuggingMutableIntSetFactory.java
+++ src/main/java/com/ibm/wala/util/intset/DebuggingMutableIntSetFactory.java
@@ -10,6 +10,7 @@
  */
 package com.ibm.wala.util.intset;
 
+import com.ibm.wala.util.debug.Assertions;
 import com.ibm.wala.util.debug.UnimplementedError;
 import javax.annotation.Nullable;
 
@@ -69,7 +70,8 @@ public class DebuggingMutableIntSetFactory implements MutableIntSetFactory<Debug
 
       return new DebuggingMutableIntSet(pr, sr);
     } else {
-      throw new IllegalArgumentException("x must be an instance of DebuggingMutableIntSet");
+      Assertions.UNREACHABLE();
+      return null;
     }
   }
 
