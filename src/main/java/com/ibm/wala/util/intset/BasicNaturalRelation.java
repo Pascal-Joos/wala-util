af0e7470d26929b2f43badffde26dd4886a66fad
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 12:27:40 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression delegateIterator is @Nullable' /home/vscode/nullness-benchmarks/wala-util/src/main/java/com/ibm/wala/util/intset/BasicNaturalRelation.java:235         int y = delegateIterator.next();

--- src/main/java/com/ibm/wala/util/intset/BasicNaturalRelation.java
+++ src/main/java/com/ibm/wala/util/intset/BasicNaturalRelation.java
@@ -231,6 +231,9 @@ public final class BasicNaturalRelation implements IBinaryNaturalRelation, Seria
     public IntPair next() {
       IntPair result = null;
       if (nextIndex == smallStore.length) {
+        if (delegateIterator == null) {
+          throw new IllegalStateException("delegateIterator is null");
+        }
         int y = delegateIterator.next();
         result = new IntPair(nextX, y);
         if (!delegateIterator.hasNext()) {
