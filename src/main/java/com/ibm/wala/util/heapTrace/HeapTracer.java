55625e5eb719dbd6b4e343b5a9b1bd4da9d423d5
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 08:46:10 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression d1 is @Nullable' /home/vscode/nullness-benchmarks/wala-util/src/main/java/com/ibm/wala/util/heapTrace/HeapTracer.java:687         return d2.getTotalSize() - d1.getTotalSize();

--- src/main/java/com/ibm/wala/util/heapTrace/HeapTracer.java
+++ src/main/java/com/ibm/wala/util/heapTrace/HeapTracer.java
@@ -687,7 +687,7 @@ public class HeapTracer {
       public int compare(Field o1, Field o2) {
         Demographics d1 = roots.get(o1);
         Demographics d2 = roots.get(o2);
-        return d2.getTotalSize() - d1.getTotalSize();
+        return d2.getTotalSize() - Nullability.castToNonnull(d1).getTotalSize();
       }
     }
   }
